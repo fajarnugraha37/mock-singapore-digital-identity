863b1ef4154b611ce0cbad16676d6caa639dcbae
Fajar Abdi Nugraha <fajar.nugraha@xtremax.com>
2024-10-18 02:55:19 +0700

    add endpoint

--- app/handlers/ndi-v2.ts
+++ app/handlers/ndi-v2.ts
@@ -1,9 +1,35 @@
-import { Hono } from "hono";
+import { Hono, type HonoRequest } from "hono";
+import { COMMON_CONTRACTS } from "@app-config/contracts.ts";
+import { oidc } from "@app-util/assertions.ts";
 
 
-export function getNDIV2Handler() {
+export function getNDIV2Handler(
+    showLoginPage = (req: HonoRequest) => (req.header('X-Show-Login-Page') || Deno.env.get('SHOW_LOGIN_PAGE')) === 'true',
+    isStateless = COMMON_CONTRACTS.IS_STATELESS
+) {
     const app = new Hono;
 
+    for (const idp of ['singPass', 'corpPass']) {
+        const profiles = oidc[idp]
+        const defaultProfile = profiles.find((p) => p.nric === Deno.env.get('MOCKPASS_NRIC')) || profiles[0];
+
+
+        app.get(`/${idp.toLowerCase()}/v2/authorize`, (c) => {
+            return c.body(c.req.url);
+        });
+        app.get(`/${idp.toLowerCase()}/v2/authorize/custom-profile`, (c) => {
+            return c.body(c.req.url);
+        });
+        app.get(`/${idp.toLowerCase()}/v2/token`, (c) => {
+            return c.body(c.req.url);
+        });
+        app.get(`/${idp.toLowerCase()}/v2/.well-known/openid-configuration`, (c) => {
+            return c.body(c.req.url);
+        });
+        app.get(`/${idp.toLowerCase()}/v2/.well-known/keys`, (c) => {
+            return c.body(c.req.url);
+        });
+    }
 
     return app;
 }
